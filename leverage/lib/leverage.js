809a09a2c33f89624bf1b93363070984a8a9d49a
Restyled.io <commits@restyled.io>
2019-10-25 21:38:54 +0000

    Restyled by prettier

--- leverage/lib/leverage.js
+++ leverage/lib/leverage.js
@@ -1,59 +1,63 @@
-const invariant = require('invariant');
-const Maker = require('@makerdao/dai');
-const Eth2DaiInstant = require('@makerdao/dai-plugin-eth2dai-instant')
-const infuraProjectId = 'c3f0f26a4c1742e0949d8eedfc47be67'; // dai.js project id
+const invariant = require("invariant");
+const Maker = require("@makerdao/dai");
+const Eth2DaiInstant = require("@makerdao/dai-plugin-eth2dai-instant");
+const infuraProjectId = "c3f0f26a4c1742e0949d8eedfc47be67"; // dai.js project id
 
 // descriptive logging
-const debug = require('debug');
+const debug = require("debug");
 const log = {
-  state : debug('leverage:state'),
-  action : debug('leverage:action'),
-  title : debug('leverage:header')
+  state: debug("leverage:state"),
+  action: debug("leverage:action"),
+  title: debug("leverage:header")
 };
 
 module.exports = async (iterations, priceFloor, principal) => {
   try {
     // connect to blockchain using infura
-    const maker = await Maker.create('http', {
-      privateKey : process.env.PRIVATE_KEY,
-      plugins : [ Eth2DaiInstant ],
-      log : true,
-      url : 'https://kovan.infura.io/v3/11465e3f27b247eb8b785c23047b29fd'
+    const maker = await Maker.create("http", {
+      privateKey: process.env.PRIVATE_KEY,
+      plugins: [Eth2DaiInstant],
+      log: true,
+      url: "https://kovan.infura.io/v3/11465e3f27b247eb8b785c23047b29fd"
     });
     await maker.authenticate();
 
     invariant(
-        iterations !== undefined && priceFloor !== undefined &&
-            principal !== undefined,
-        'Not all parameters (iterations, priceFloor, principal) were received');
+      iterations !== undefined &&
+        priceFloor !== undefined &&
+        principal !== undefined,
+      "Not all parameters (iterations, priceFloor, principal) were received"
+    );
 
-    log.title('Creating a leveraged cdp with the following parameters:');
+    log.title("Creating a leveraged cdp with the following parameters:");
     log.title(`Iterations: ${iterations}`);
     log.title(`Price Floor: $${priceFloor}`);
     log.title(`Principal: ${principal} ETH`);
 
     // await maker.authenticate();
-    const liquidationRatio = await maker.service('cdp').getLiquidationRatio();
-    const priceEth = (await maker.service('price').getEthPrice()).toNumber();
+    const liquidationRatio = await maker.service("cdp").getLiquidationRatio();
+    const priceEth = (await maker.service("price").getEthPrice()).toNumber();
 
     // Getting the exchange
-    let oasis = await maker.service('exchange');
-    console.log('Getting the Exchange: ', oasis)
+    let oasis = await maker.service("exchange");
+    console.log("Getting the Exchange: ", oasis);
 
     log.state(`Liquidation ratio: ${liquidationRatio}`);
     log.state(`Current price of ETH: ${priceEth}`);
 
-    invariant(priceEth > priceFloor,
-              'Price floor must be below the current oracle price');
+    invariant(
+      priceEth > priceFloor,
+      "Price floor must be below the current oracle price"
+    );
 
-    log.action('opening CDP...');
+    log.action("opening CDP...");
     const cdp = await maker.openCdp();
     const id = await cdp.id;
     log.state(`CDP ID: ${id}`);
 
     // calculate a collateralization ratio that will achieve the given price
     // floor
-    const collatRatio = priceEth * liquidationRatio / priceFloor;
+    const collatRatio = (priceEth * liquidationRatio) / priceFloor;
     log.state(`Target ratio: ${collatRatio}`);
 
     // lock up all of our principal
@@ -66,15 +70,15 @@ module.exports = async (iterations, priceFloor, principal) => {
 
     // calculate how much Dai we need to draw in order
     // to achieve the desired collateralization ratio
-    let drawAmt = Math.floor(principal * priceEth / collatRatio);
+    let drawAmt = Math.floor((principal * priceEth) / collatRatio);
     const drawTx = cdp.drawDai(drawAmt);
-    await maker.service('transactionManager').confirm(drawTx);
+    await maker.service("transactionManager").confirm(drawTx);
     log.action(`drew ${drawAmt} Dai`);
 
     // do `iterations` round trip(s) to the exchange
     for (let i = 0; i < iterations; i++) {
       // exchange the drawn Dai for W-ETH
-      let tx = await maker.service('exchange').sellDai(drawAmt, Maker.WETH);
+      let tx = await maker.service("exchange").sellDai(drawAmt, Maker.WETH);
 
       // observe the amount of W-ETH received from the exchange
       // by calling `fillAmount` on the returned transaction object
@@ -87,14 +91,16 @@ module.exports = async (iterations, priceFloor, principal) => {
 
       // calculate how much Dai we need to draw in order to
       // re-attain our desired collateralization ratio
-      drawAmt = Math.floor(returnedWeth.toNumber() * priceEth / collatRatio);
+      drawAmt = Math.floor((returnedWeth.toNumber() * priceEth) / collatRatio);
       await cdp.drawDai(drawAmt);
       log.action(`drew ${drawAmt} Dai`);
     }
 
     // get the final state of our CDP
-    const [pethCollateral, debt] = await Promise.all(
-        [ cdp.getCollateralValue(Maker.PETH), cdp.getDebtValue() ]);
+    const [pethCollateral, debt] = await Promise.all([
+      cdp.getCollateralValue(Maker.PETH),
+      cdp.getDebtValue()
+    ]);
 
     const cdpState = {
       initialPethCollateral,
@@ -104,12 +110,12 @@ module.exports = async (iterations, priceFloor, principal) => {
       principal,
       iterations,
       priceFloor,
-      finalDai : drawAmt
+      finalDai: drawAmt
     };
 
     log.state(`Created CDP: ${JSON.stringify(cdpState)}`);
     return cdpState;
   } catch (error) {
-    console.log('ERROR OCCURRED!', error)
+    console.log("ERROR OCCURRED!", error);
   }
 };
--- leverage/test/leverage.test.js
+++ leverage/test/leverage.test.js
@@ -1,10 +1,7 @@
-const leverage = require('../lib/leverage');
+const leverage = require("../lib/leverage");
 
-test(
-    'leveraged cdp should end up with more eth than it started with',
-    async () => {
-      const state = await leverage(0.1, 50, 0.01);
-      console.log(state.pethCollateral.gt(state.initialPethCollateral));
-      expect(state.pethCollateral.gt(state.initialPethCollateral)).toBeTruthy();
-    },
-    240000);
+test("leveraged cdp should end up with more eth than it started with", async () => {
+  const state = await leverage(0.1, 50, 0.01);
+  console.log(state.pethCollateral.gt(state.initialPethCollateral));
+  expect(state.pethCollateral.gt(state.initialPethCollateral)).toBeTruthy();
+}, 240000);
